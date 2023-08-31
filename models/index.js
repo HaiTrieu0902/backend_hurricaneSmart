5e2791fbc6237f4fb9795d434ab2f056070b6ae5
Bùi Hải Triều <trieuql159@gmail.com>
2023-08-31 10:29:50 +0700

    Lam lai tu dau, tu day xa hoi di len :((

--- models/index.js
+++ models/index.js
@@ -1,11 +1,4 @@
 const UserModel = require('./temp/User');
-const MarriageModel = require('./temp/Marriage');
-const DepartmentModel = require('./temp/Department');
-const EmployeeModel = require('./temp/Employee');
-
 module.exports = {
     UserModel,
-    MarriageModel,
-    DepartmentModel,
-    EmployeeModel,
 };
