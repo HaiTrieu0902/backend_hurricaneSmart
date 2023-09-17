1f7693181c903e5ffa9f40eecf918c5b248daf7f
Bùi Hải Triều <trieuql159@gmail.com>
2023-09-17 16:39:25 +0700

    "hihih"

--- controllers/Report/ReportController.js
+++ controllers/Report/ReportController.js
@@ -52,7 +52,81 @@ const reportController = {
 
     getReportMonthOrYearTransaction: async (req, res) => {
         try {
-            return res.status(200).json({ message: 'getReportMonthOrYearTransaction' });
+            const { userId, month, year } = req.query;
+            const user = await UserModel.findOne({ user_id: userId });
+            if (!user) {
+                return res.status(401).json({ error: 'Not found user ID, please try it again!' });
+            }
+            // if (month) {
+            //     const startDate = moment(`${year}-${month}-01`, 'YYYY-MM-DD');
+            //     const endDate = moment(startDate).endOf('month');
+
+            //     const transactions = await TransactionModel.find({
+            //         user_id: userId,
+            //         date: {
+            //             $gte: startDate.toDate(),
+            //             $lte: endDate.toDate(),
+            //         },
+            //     }).select('-_id -__v -user_id');
+            //     return res.status(200).json({ data: transactions });
+            // } else {
+            //     const transactions = await TransactionModel.find({
+            //         user_id: userId,
+            //     }).select('-_id -__v -user_id');
+            //     return res.status(200).json({ data: transactions });
+            // }
+            let transactions;
+            if (month) {
+                const startDate = moment(`${year}-${month}-01`, 'YYYY-MM-DD');
+                const endDate = moment(startDate).endOf('month');
+
+                transactions = await TransactionModel.find({
+                    user_id: userId,
+                    date: {
+                        $gte: startDate.toDate(),
+                        $lte: endDate.toDate(),
+                    },
+                }).select('-_id -__v -user_id');
+            } else {
+                const startOfYear = moment(`${year}-01-01`, 'YYYY-MM-DD');
+                const endOfYear = moment(startOfYear).endOf('year');
+
+                transactions = await TransactionModel.find({
+                    user_id: userId,
+                    date: {
+                        $gte: startOfYear.toDate(),
+                        $lte: endOfYear.toDate(),
+                    },
+                }).select('-_id -__v -user_id');
+            }
+            const totalSpent = transactions.reduce((total, transaction) => {
+                return total + transaction.amount;
+            }, 0);
+
+            // Nhóm các giao dịch theo category_key và tính tổng cho mỗi category_key
+            const groupedTransactions = transactions.reduce((acc, transaction) => {
+                const { category_key, amount } = transaction;
+
+                console.log('hihi', acc);
+                if (!acc[category_key]) {
+                    acc[category_key] = { category_key, total: 0 };
+                }
+                acc[category_key].total += amount;
+                return acc;
+            }, {});
+
+            const data = Object.values(groupedTransactions);
+
+            // Tạo đối tượng JSON phản hồi
+            const response = {
+                message: 'Success',
+                month: parseInt(month),
+                year: parseInt(year),
+                total_spent: totalSpent,
+                data: data,
+            };
+
+            return res.status(200).json(response);
         } catch (error) {
             res.status(500).json(error);
         }
